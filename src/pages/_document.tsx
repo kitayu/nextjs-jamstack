918faf2544f3760374ae84d305f585010d81f80d
Yoshihisa Kitayuguchi <yoshihisa.kitayuguchi@gmail.com>
2024-02-04 17:57:52 +0900

    Next.jsでWebアプリを作ってみよう（ハンズオン基礎編）を実施

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,21 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
-export default function Document() {
+const Index = () => {
   return (
-    <Html lang="en">
-      <Head />
+    <Html>
+      <Head>
+        <link
+          href={
+            'https://fonts.googleapis.com/css2?family=noto+Sans+JP:wght@400;500;700&display=swap'
+          }
+          rel="stylesheet"
+        ></link>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
-}
+  );
+};
+export default Index;
